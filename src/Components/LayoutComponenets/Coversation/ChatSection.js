2eacfecbd0ea060b5806dd469339e35167df8b40
sharaizk <64202206+sharaizk@users.noreply.github.com>
2021-09-20 20:24:33 +0500

    Add Friend - Fully Functioning

--- src/Components/LayoutComponenets/Coversation/ChatSection.js
+++ src/Components/LayoutComponenets/Coversation/ChatSection.js
@@ -3,17 +3,7 @@ import React from 'react'
 const ChatSection = () => {
     return (
         <div className="chat-container">
-            <label className="myMessage">
-            OO KI HAAL AY?
-            </label>
-            
-            <label className="otherMessage">
-            Main theek aan ustad
-            </label>
-
-            <label className="myMessage">
-            Chal fer chuti kar
-            </label>
+            Chats will appear here
         </div>
     )
 }
